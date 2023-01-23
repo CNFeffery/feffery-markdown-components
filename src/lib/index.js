63d8467877e3eeda80c5298875ce5caa6630c4d6
CNFeffery <fefferypzy@gmail.com>
2023-01-23 21:45:49 +0800

    feat: 新增语法高亮组件FefferySyntaxHighlighter（移植自fuc）

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,5 +1,6 @@
 /* eslint-disable import/prefer-default-export */
 import FefferyMarkdown from './components/FefferyMarkdown.react';
+import FefferySyntaxHighlighter from './components/FefferySyntaxHighlighter.react';
 
 /* 
 忽略部分设计React中规范的console警告信息
@@ -7,22 +8,23 @@ import FefferyMarkdown from './components/FefferyMarkdown.react';
 1. 数组推导形成的组件，每个子组件需要唯一的key
 2. 在原生html元素中携带小驼峰命名法的props
 */
-try {
-    consoleErrorBackup
-} catch (e) {
-    const consoleErrorBackup = console.error;
-    console.error = (msg) => {
-        const supressedWarnings = [
-            'Each child in a list should have a unique',
-            'React does not recognize the'
-        ];
+// try {
+//     consoleErrorBackup
+// } catch (e) {
+//     const consoleErrorBackup = console.error;
+//     console.error = (msg) => {
+//         const supressedWarnings = [
+//             'Each child in a list should have a unique',
+//             'React does not recognize the'
+//         ];
 
-        if (!supressedWarnings.some(entry => msg.includes && msg.includes(entry))) {
-            consoleErrorBackup(msg);
-        }
-    };
-}
+//         if (!supressedWarnings.some(entry => msg.includes && msg.includes(entry))) {
+//             consoleErrorBackup(msg);
+//         }
+//     };
+// }
 
 export {
-    FefferyMarkdown
+    FefferyMarkdown,
+    FefferySyntaxHighlighter
 };
