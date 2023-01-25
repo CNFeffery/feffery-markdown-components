4f6e6bbcb70c7ea675aa2f3a7e128eb28eab715b
CNFeffery <fefferypzy@gmail.com>
2023-01-25 22:11:58 +0800

    chore: 更新dash最低依赖版本至2.8.0

--- feffery_markdown_components/_imports_.py
+++ feffery_markdown_components/_imports_.py
@@ -1,5 +1,7 @@
 from .FefferyMarkdown import FefferyMarkdown
+from .FefferySyntaxHighlighter import FefferySyntaxHighlighter
 
 __all__ = [
-    "FefferyMarkdown"
+    "FefferyMarkdown",
+    "FefferySyntaxHighlighter"
 ]
--- usage.py
+++ usage.py
@@ -10,6 +10,114 @@ app = dash.Dash(
 
 app.layout = html.Div(
     [
+        fmc.FefferySyntaxHighlighter(
+            wrapLongLines=False,
+            codeString="""
+import React from "react";
+import uniquePropHOC from "./lib/unique-prop-hoc";
+
+// this comment is here to demonstrate an extremely long line length, well beyond what you should probably allow in your own code, though sometimes you'll be highlighting code you can't refactor, which is unfortunate but should be handled gracefully
+
+class Expire extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { component: props.children }
+    }
+    componentDidMount() {
+        setTimeout(() => {
+            this.setState({
+                component: null
+            });
+        }, this.props.time || this.props.seconds * 1000);
+    }
+    render() {
+        return this.state.component;
+    }
+}
+
+export default uniquePropHOC(["time", "seconds"])(Expire);
+
+""",
+            language='javascript',
+            codeBlockStyle={
+                'maxHeight': '300px'
+            },
+        ),
+
+        fmc.FefferyMarkdown(
+            codeBlockStyle={
+                'maxHeight': '300px'
+            },
+            markdownStr="""
+```python
+import dash
+from dash import html
+import feffery_markdown_components as fmc
+
+app = dash.Dash(__name__)
+
+raw_markdown = '''
+### 1 标题
+
+在文字写书写不同数量的`#`可以完成不同的标题，如下：
+
+# 一级标题
+
+## 二级标题
+
+### 三级标题
+
+### 2 无序列表
+
+无序列表的使用，在符号`-`后加空格使用。如下：
+
+- 无序列表 1
+- 无序列表 2
+- 无序列表 3
+
+如果要控制列表的层级，则需要在符号`-`前使用空格。如下：
+
+- 无序列表 1
+- 无序列表 2
+- 无序列表 2.1
+- 无序列表 2.2
+
+### 3 有序列表
+
+有序列表的使用，在数字及符号`.`后加空格后输入内容，如下：
+
+1. 有序列表 1
+2. 有序列表 2
+3. 有序列表 3
+'''
+
+app.layout = html.Div(
+[
+fmc.FefferyMarrkdown(
+markdownStr=raw_markdown
+)
+]
+)
+
+if __name__ == '__main__':
+app.run()
+```
+"""
+        ),
+
+        fmc.FefferyMarkdown(
+            codeStyle={
+                'fontSize': '20px'
+            },
+            markdownStr='''
+```python
+import dash
+from dash import html
+import feffery_markdown_components as fmc
+```
+'''
+        ),
+
         fmc.FefferyMarkdown(
             markdownStr=r'''
 　　这是行LaTeX公式示例：$E=mc^2$
