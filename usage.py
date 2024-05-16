4e9b3ddac84e0152b28e58205a2910c2274607b9
CNFeffery <fefferypzy@gmail.com>
2024-05-16 16:39:58 +0800

    chore: 新增ruff.toml配置文件

--- usage.py
+++ usage.py
@@ -3,15 +3,12 @@ from dash import html
 import feffery_markdown_components as fmc
 from dash.dependencies import Input, Output
 
-app = dash.Dash(
-    __name__,
-    suppress_callback_exceptions=True
-)
+app = dash.Dash(__name__, suppress_callback_exceptions=True)
 
 app.layout = html.Div(
     [
         fmc.FefferySyntaxHighlighter(
-            codeString='''
+            codeString="""
 import dash
 from dash import html
 import feffery_markdown_components as fmc
@@ -21,17 +18,14 @@ app = dash.Dash(
     __name__,
     suppress_callback_exceptions=True
 )
-''',
+""",
             language='python',
             addedLineNumbers=[2, 3, 4],
             removedLineNumbers=[5, 6, 7],
-            codeTheme='one-light'
+            codeTheme='one-light',
         )
     ],
-    style={
-        'width': '1200px',
-        'margin': '0 auto'
-    }
+    style={'width': '1200px', 'margin': '0 auto'},
 )
 
 
