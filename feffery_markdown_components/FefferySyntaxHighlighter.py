50a226b8ea8d2833370bf2c0125ffd4c2ff4b45b
CNFeffery <fefferypzy@gmail.com>
2024-11-27 17:23:25 +0800

    chore: 重构FefferySyntaxHighlighter源码参数注释

--- feffery_markdown_components/FefferySyntaxHighlighter.py
+++ feffery_markdown_components/FefferySyntaxHighlighter.py
@@ -9,33 +9,51 @@ class FefferySyntaxHighlighter(Component):
 
 Keyword arguments:
 
-- id (string; optional)
+- id (string; optional):
+    组件唯一id.
 
-- key (string; optional)
+- key (string; optional):
+    对当前组件的`key`值进行更新，可实现强制重绘当前组件的效果.
 
-- codeString (string; required)
+- codeString (string; required):
+    代码内容字符串.
 
-- language (string; required)
+- language (string; required):
+    必填，代码语言.
 
-- codeTheme (a value equal to: 'a11y-dark', 'atom-dark', 'coldark-cold', 'coldark-dark', 'coy', 'coy-without-shadows', 'darcula', 'dracula', 'nord', 'okaidia', 'prism', 'solarizedlight', 'twilight', 'duotone-sea', 'duotone-dark', 'duotone-light', 'duotone-space', 'gh-colors', 'gruvbox-dark', 'material-dark', 'night-owl', 'one-light', 'pojoaque', 'solarized-dark-atom', 'synthwave84', 'z-touch'; default 'gh-colors')
+- codeTheme (a value equal to: 'a11y-dark', 'atom-dark', 'coldark-cold', 'coldark-dark', 'coy', 'coy-without-shadows', 'darcula', 'dracula', 'nord', 'okaidia', 'prism', 'solarizedlight', 'twilight', 'duotone-sea', 'duotone-dark', 'duotone-light', 'duotone-space', 'gh-colors', 'gruvbox-dark', 'material-dark', 'night-owl', 'one-light', 'pojoaque', 'solarized-dark-atom', 'synthwave84', 'z-touch'; default 'gh-colors'):
+    代码高亮主题，可选项有`'a11y-dark'`、`'atom-dark'`、`'coldark-cold'`、`'coldark-dark'`、`'coy'`、
+    `'coy-without-shadows'`、`'darcula'`、`'dracula'`、`'nord'`、`'okaidia'`、`'prism'`、`'solarizedlight'`、
+    `'twilight'`、`'duotone-sea'`、`'duotone-dark'`、`'duotone-light'`、`'duotone-space'`、`'gh-colors'`、
+    `'gruvbox-dark'`、`'material-dark'`、`'night-owl'`、`'one-light'`、`'pojoaque'`、`'solarized-dark-atom'`、
+    `'synthwave84'`、`'z-touch'`  默认值：`'gh-colors'`.
 
-- codeBlockStyle (dict; optional)
+- codeBlockStyle (dict; optional):
+    代码块容器css样式.
 
-- codeStyle (dict; optional)
+- codeStyle (dict; optional):
+    代码内容css样式.
 
-- showLineNumbers (boolean; default True)
+- showLineNumbers (boolean; default True):
+    代码块是否显示行号  默认值：`True`.
 
-- showCopyButton (boolean; default True)
+- showCopyButton (boolean; default True):
+    代码块是否在右上角显示复制按钮  默认值：`True`.
 
-- wrapLongLines (boolean; default False)
+- wrapLongLines (boolean; default False):
+    是否允许超长代码行自动换行  默认值：`False`.
 
-- addedLineNumbers (list of numbers; optional)
+- addedLineNumbers (list of numbers; optional):
+    配置需要渲染为差异比较新增行的代码行下标列表  默认值：`[]`.
 
-- removedLineNumbers (list of numbers; optional)
+- removedLineNumbers (list of numbers; optional):
+    配置需要渲染为差异比较移除行的代码行下标列表  默认值：`[]`.
 
-- addedLineStyle (dict; default { backgroundColor: '#e6ffec' })
+- addedLineStyle (dict; default { backgroundColor: '#e6ffec' }):
+    差异比较新增行css样式.
 
-- removedLineStyle (dict; default { backgroundColor: '#ffebe9' })
+- removedLineStyle (dict; default { backgroundColor: '#ffebe9' }):
+    差异比较移除行css样式.
 
 - loading_state (dict; optional)
 
--- src/jl/''_fefferysyntaxhighlighter.jl
+++ src/jl/''_fefferysyntaxhighlighter.jl
@@ -8,25 +8,35 @@ export ''_fefferysyntaxhighlighter
 A FefferySyntaxHighlighter component.
 代码语法高亮组件FefferySyntaxHighlighter
 Keyword arguments:
-- `id` (String; optional)
-- `addedLineNumbers` (Array of Reals; optional)
-- `addedLineStyle` (Dict; optional)
-- `codeBlockStyle` (Dict; optional)
-- `codeString` (String; required)
-- `codeStyle` (Dict; optional)
-- `codeTheme` (a value equal to: 'a11y-dark', 'atom-dark', 'coldark-cold', 'coldark-dark', 'coy', 'coy-without-shadows', 'darcula', 'dracula', 'nord', 'okaidia', 'prism', 'solarizedlight', 'twilight', 'duotone-sea', 'duotone-dark', 'duotone-light', 'duotone-space', 'gh-colors', 'gruvbox-dark', 'material-dark', 'night-owl', 'one-light', 'pojoaque', 'solarized-dark-atom', 'synthwave84', 'z-touch'; optional)
-- `key` (String; optional)
-- `language` (String; required)
+- `id` (String; optional): 组件唯一id
+- `addedLineNumbers` (Array of Reals; optional): 配置需要渲染为差异比较新增行的代码行下标列表
+默认值：`[]`
+- `addedLineStyle` (Dict; optional): 差异比较新增行css样式
+- `codeBlockStyle` (Dict; optional): 代码块容器css样式
+- `codeString` (String; required): 代码内容字符串
+- `codeStyle` (Dict; optional): 代码内容css样式
+- `codeTheme` (a value equal to: 'a11y-dark', 'atom-dark', 'coldark-cold', 'coldark-dark', 'coy', 'coy-without-shadows', 'darcula', 'dracula', 'nord', 'okaidia', 'prism', 'solarizedlight', 'twilight', 'duotone-sea', 'duotone-dark', 'duotone-light', 'duotone-space', 'gh-colors', 'gruvbox-dark', 'material-dark', 'night-owl', 'one-light', 'pojoaque', 'solarized-dark-atom', 'synthwave84', 'z-touch'; optional): 代码高亮主题，可选项有`'a11y-dark'`、`'atom-dark'`、`'coldark-cold'`、`'coldark-dark'`、`'coy'`、
+`'coy-without-shadows'`、`'darcula'`、`'dracula'`、`'nord'`、`'okaidia'`、`'prism'`、`'solarizedlight'`、
+`'twilight'`、`'duotone-sea'`、`'duotone-dark'`、`'duotone-light'`、`'duotone-space'`、`'gh-colors'`、
+`'gruvbox-dark'`、`'material-dark'`、`'night-owl'`、`'one-light'`、`'pojoaque'`、`'solarized-dark-atom'`、
+`'synthwave84'`、`'z-touch'`
+默认值：`'gh-colors'`
+- `key` (String; optional): 对当前组件的`key`值进行更新，可实现强制重绘当前组件的效果
+- `language` (String; required): 必填，代码语言
 - `loading_state` (optional): . loading_state has the following type: lists containing elements 'is_loading', 'prop_name', 'component_name'.
 Those elements have the following types:
   - `is_loading` (Bool; optional): Determines if the component is loading or not
   - `prop_name` (String; optional): Holds which property is loading
   - `component_name` (String; optional): Holds the name of the component that is loading
-- `removedLineNumbers` (Array of Reals; optional)
-- `removedLineStyle` (Dict; optional)
-- `showCopyButton` (Bool; optional)
-- `showLineNumbers` (Bool; optional)
-- `wrapLongLines` (Bool; optional)
+- `removedLineNumbers` (Array of Reals; optional): 配置需要渲染为差异比较移除行的代码行下标列表
+默认值：`[]`
+- `removedLineStyle` (Dict; optional): 差异比较移除行css样式
+- `showCopyButton` (Bool; optional): 代码块是否在右上角显示复制按钮
+默认值：`true`
+- `showLineNumbers` (Bool; optional): 代码块是否显示行号
+默认值：`true`
+- `wrapLongLines` (Bool; optional): 是否允许超长代码行自动换行
+默认值：`false`
 """
 function ''_fefferysyntaxhighlighter(; kwargs...)
         available_props = Symbol[:id, :addedLineNumbers, :addedLineStyle, :codeBlockStyle, :codeString, :codeStyle, :codeTheme, :key, :language, :loading_state, :removedLineNumbers, :removedLineStyle, :showCopyButton, :showLineNumbers, :wrapLongLines]
